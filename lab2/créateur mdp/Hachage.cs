7b12d376d1d2fef9c8396906d4d4e769c2960c6a
Zembrax <157525041+Zembrax@users.noreply.github.com>
2024-09-12 22:22:45 -0400

    Add files via upload

---
+++ lab2/créateur mdp/Hachage.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HachageMDP
+{
+    internal class Hachage
+    {
+        public static string HacherMotDePasse(string motDePasse)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(motDePasse));
+
+                string hash = BitConverter.ToString(bytes).Replace("-", "").ToLower();
+
+                return hash;
+            }
+        }
+    }
+}
---
+++ lab2/créateur mdp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HachageMDP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inputFile = "Liste_MDP.txt";
+            string outputFile = "SortiEncripter.txt";
+
+
+            string[] mDP = File.ReadAllLines(inputFile);
+
+
+            using (StreamWriter writer = new StreamWriter(outputFile))
+            {
+                foreach (string password in mDP)
+                {
+
+                    string hash = Hachage.HacherMotDePasse(password);
+
+
+                    writer.WriteLine(hash);
+                }
+            }
+        }
+    }
+}
